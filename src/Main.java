43ce2830a7b6db8eb68ee2a6252a27307be4633a
max <kabinidi@mail.ru>
2022-10-23 13:29:48 +0300

    Answer 1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int numberInt = 1050050;
+        byte numberBy = 10;
+        short numberSh = 17000;
+        long numberLo = 123456789;
+        System.out.println ("Значение переменной numberInt с типом int равно " + numberInt);
+        System.out.println ("Значение переменной numberBy с типом byte равно " + numberBy);
+        System.out.println ("Значение переменной numberSh с типом short равно " + numberSh);
+        System.out.println ("Значение переменной numberLo с типом long равно " + numberLo);
+
+
+
+
+
+
+
+
+
+
+    }
+}
